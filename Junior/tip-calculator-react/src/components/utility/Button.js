d5add3d6d1f5d2dc4a4e72ea16634514c25c5968
jackson.lu02@myhunter.cuny.edu <jackson.lu02@myhunter.cuny.edu>
2021-09-06 13:15:17 -0400

    minor tweaks on padding,fontsize and colors

--- Junior/tip-calculator-react/src/components/utility/Button.js
+++ Junior/tip-calculator-react/src/components/utility/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({text, className}) => {
     return (
-        <button className={className}>
+        <button disabled className={className}>
             {text}
         </button>
     )
