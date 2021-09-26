8d462eef4fb728041fd3dc80ef0ab149e39db9ab
jackson.lu02@myhunter.cuny.edu <jackson.lu02@myhunter.cuny.edu>
2021-09-26 14:26:07 -0400

    nav bar changed after exits header using intersection observer

--- Junior/loopstudios-landing-page/src/index.js
+++ Junior/loopstudios-landing-page/src/index.js
@@ -3,11 +3,29 @@ const navOpen = document.querySelector(".nav-open");
 const navClose = document.querySelector(".nav-close");
 
 navOpen.addEventListener("click", (e) => {
-    nav.classList.add("show");
-    document.body.style.overflow = "hidden";
-})
+	nav.classList.add("show");
+	document.body.style.overflow = "hidden";
+});
 
 navClose.addEventListener("click", (e) => {
-    nav.classList.remove("show");
-    document.body.style.overflow = "visible";
-})
+	nav.classList.remove("show");
+	document.body.style.overflow = "visible";
+});
+
+const header = document.querySelector("header");
+const headerOptions = {
+	root: null,
+	rootMargin: "-130px 0px 0px 0px",
+};
+
+const headerObserver = new IntersectionObserver((entries, headerObserver) => {
+	entries.forEach((entry) => {
+		if (entry.isIntersecting) {
+			nav.classList.remove("scrolled");
+		} else {
+			nav.classList.add("scrolled");
+		}
+	});
+}, headerOptions);
+
+headerObserver.observe(header);
